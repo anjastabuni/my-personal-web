69dc3b4db612f413da24b9c8b8559e0caecd2fb5
username_anjastabuni <reblytabuni2004@gmail.com>
2023-05-11 17:11:16 +0700

    update mode

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// buat mode
+var icon = document.getElementById("mode");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-thema");
+  if (document.body.classList.contains("dark-thema")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+};
+
 function github() {
   window.location.href = "https://github.com/anjastabuni";
 }
